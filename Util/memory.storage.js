1d04ea42a6e099052a2fe3d47bb22bfffcd794fe
omar95-pero <omarpero85@gmail.com>
2022-05-18 15:32:35 +0200

    MemoryStorage

---
+++ Util/memory.storage.js
@@ -0,0 +1,24 @@
+const MemoryStorage = require("memorystorage");
+
+const store = new MemoryStorage('note-app');
+
+exports.getKeys = (store)=>{
+    var keys = [];
+    for (var i=0; i<store.length; i++) {
+        var key = store.key(i);
+        keys.push(key)
+    }
+    return keys
+}
+exports.getValues = (store)=>{
+    var values = [];
+    for (var i=0; i<store.length; i++) {
+        var key = store.key(i);
+        var value = store.getItem(key);
+        values.push(value)
+    }
+    return values
+}
+
+
+exports.store = store;
--- controller/noteController.js
+++ controller/noteController.js
@@ -1,9 +1,16 @@
 const generator = require("../Util/generator");
+const memStrg = require("../Util/memory.storage");
 
 
 exports.getAllNotes = (req,res)=>{
     let new_1 = generator.generate();
-res.send("Get All Notes From Controller"+new_1);
+    let value_1 = memStrg.store.setItem(new_1,'item_1');
+    let new_2 = generator.generate();
+    let value_2 = memStrg.store.setItem(new_2,'item_2');
+    let keys = memStrg.getKeys(memStrg.store);
+    let values = memStrg.getValues(memStrg.store);
+res.send("all values ........"+JSON.stringify(keys));
+console.log(JSON.stringify(values));
 }
 
 exports.saveNote = (req,res)=>{
